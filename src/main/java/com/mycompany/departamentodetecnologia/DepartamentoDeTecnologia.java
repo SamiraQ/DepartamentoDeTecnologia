ccfb79f28f2c0c93223ae8467757da3df063ea80
LeonelJi <lj924618@gmail.com>
2024-11-26 15:59:15 -0500

    Hola

---
+++ src/main/java/com/mycompany/departamentodetecnologia/DepartamentoDeTecnologia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.departamentodetecnologia;
+
+/**
+ *
+ * @author User
+ */
+public class DepartamentoDeTecnologia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
