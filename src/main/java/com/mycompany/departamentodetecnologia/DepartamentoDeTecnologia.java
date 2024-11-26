48c8720ad8c8046e2b2aa636d11098e57eaafe0d
LeonelJi <lj924618@gmail.com>
2024-11-26 16:04:43 -0500

    Hice un cambio

--- src/main/java/com/mycompany/departamentodetecnologia/DepartamentoDeTecnologia.java
+++ src/main/java/com/mycompany/departamentodetecnologia/DepartamentoDeTecnologia.java
@@ -12,5 +12,6 @@ public class DepartamentoDeTecnologia {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hice un cambio");
     }
 }
